fde62acc368619b0c4481673d1a6ef55d101fe09
Vasil Genov <V.Genov@office.credoweb.bg>
2017-07-05 17:44:00 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import {Http} from '@angular/http';
+import {Component} from '@angular/core';
+
+import {Subject} from 'rxjs/Subject';
+import 'rxjs/add/operator/distinct';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+    title = 'GitHub Instant Search';
+    searchTerm: string;
+    results;
+    latestSearch = new Subject<string>();
+
+    constructor(public http: Http) {
+        this.results = this.latestSearch
+            .debounceTime(500)
+            .distinct()
+            .filter(term => !!term)
+            .switchMap(term =>
+                 this.http.get(`https://api.github.com/search/repositories?q=${term}&sort=stars&order=desc`)
+                    .map(res => res.json()
+                        .items.map(item => item)));
+    }
+
+    newSearch(term) {
+        this.latestSearch.next(term);
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
-import { AppComponent } from './app.component';
+import {MaterialModule} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import {AppComponent} from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        MaterialModule,
+        BrowserAnimationsModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+}
